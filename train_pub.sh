f670e01ca23d7dfdddf2986ceb69d60db5259412
aaravamudan2014 <aaravamudan2014@my.fit.edu>
2021-09-22 10:04:21 -0400

    Update train_pub.sh

--- train_pub.sh
+++ train_pub.sh
@@ -3,11 +3,13 @@
 nsplits=1
 nseeds=1
 firstseed=300
+huc="01"
+
 
 gpucount=-1
 for (( seed = $firstseed ; seed < $((nseeds+$firstseed)) ; seed++ )); do
 
-  python3 main.py --n_splits=$nsplits --seed=$seed create_splits --experiment='E1'
+  python3 main.py --n_splits=$nsplits --seed=$seed create_splits --experiment='E1' --huc=$huc
   wait
 
   for ((split = 0 ; split < $nsplits ; split++ )); do  
@@ -19,7 +21,7 @@ for (( seed = $firstseed ; seed < $((nseeds+$firstseed)) ; seed++ )); do
     if [ "$1" = "lstm" ]
     then
       outfile="reports/pub_lstm_extended_nldas.$seed.$split.out"
-      python3 main.py --gpu=$gpu --experiment='E1' --no_static=False --concat_static=True --split=$split --split_file="data/kfold_splits_seed$seed.p" train 
+      python3 main.py --gpu=$gpu --experiment='E1' --no_static=False --huc=$huc --concat_static=True --split=$split --split_file="data/kfold_splits_seed$seed.p" train 
       # > $outfile &
 
     else
@@ -34,4 +36,6 @@ for (( seed = $firstseed ; seed < $((nseeds+$firstseed)) ; seed++ )); do
 
   done
 done
+
+python3 main.py --n_splits=$nsplits --seed=$seed evaluate --experiment='E1' --huc=$huc --split_file="data/kfold_splits_seed$seed.p"
 
