5c1211e6049473dbbbe08623e3170211a24c1e99
aaravamudan2014 <aaravamudan2014@my.fit.edu>
2021-10-06 18:57:43 -0400

    Update train_pub.sh

--- train_pub.sh
+++ train_pub.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 nsplits=1
-nseeds=1
+nseeds=10
 firstseed=300
 
 gpucount=-1
@@ -25,9 +25,8 @@ for (( seed = $firstseed ; seed < $((nseeds+$firstseed)) ; seed++ )); do
     then
 
       outfile="reports/pub_lstm_extended_nldas.$seed.$split.out"
-      python3 main.py --gpu=$gpu --no_static=False --experiment=$exp --huc=$huc --concat_static=True --split=$split --split_file="data/kfold_splits_seed$seed.p" train 
-      # > $outfile &
-
+      python3 main.py --gpu=$gpu --no_static=False --experiment=$exp --huc=$huc --concat_static=True --split=$split --split_file="data/kfold_splits_seed$seed.p" train > $outfile &
+      
     else
       echo bad model choice
       exit
@@ -42,5 +41,29 @@ for (( seed = $firstseed ; seed < $((nseeds+$firstseed)) ; seed++ )); do
 done
 
 
-python3 main.py --n_splits=$nsplits --seed=$firstseed --huc=$huc --experiment=$exp --concat_static=True --run_dir="runs/${run_dir}" --split_file="data/kfold_splits_seed$firstseed.p" evaluate
-wait
+for (( seed = $firstseed ; seed < $((nseeds+$firstseed)) ; seed++ )); do
+  for ((split = 0 ; split < $nsplits ; split++ )); do  
+
+    gpucount=$(($gpucount + 1))
+    gpu=$(($gpucount % 3))
+    echo $seed $gpucount $gpu
+
+    if [ "$1" = "lstm" ]
+    then
+
+      outfile="reports/pub_lstm_extended_nldas.$seed.$split.out"
+      python3 main.py --n_splits=$nsplits --seed=$firstseed --huc=$huc --experiment=$exp --concat_static=True --run_dir="runs/${run_dir}" --split_file="data/kfold_splits_seed$firstseed.p" evaluate > $outfile &
+      
+    else
+      echo bad model choice
+      exit
+    fi
+
+    if [ $gpu -eq 2 ]
+    then
+      wait
+    fi
+
+  done
+done
+
